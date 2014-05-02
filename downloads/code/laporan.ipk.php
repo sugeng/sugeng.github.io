3a46c7d48ff5d03889263346b46b4cca8d65c735
sugeng <sugeng.doyle@gmail.com>
2014-05-02 15:33:02 +0700

    Site updated at 2014-05-02 08:33:02 UTC

--- downloads/code/laporan.ipk.php
+++ downloads/code/laporan.ipk.php
@@ -77,18 +77,30 @@ function createFile()
 {
 
 	// Variable..
-	$json   = contents();
-	$header = json_decode(contentHeader());
-	$_lf  = "\n";
-  	$line = str_pad('-', 94, '-') . $_lf;
+  $json   = contents();
+  $header = json_decode(contentHeader());
+  $_lf    = "\n";
+  $line   = str_pad('-', 94, '-') . $_lf;
 
   	// HEADER LAPORAN
 	$hdr  = str_pad("*** Rekapitulasi History Nilai ***", 94, ' ', STR_PAD_BOTH) . $_lf.$_lf;
 
-	$hdr .= str_pad("NIM & NAMA", 15, ' ') .str_pad(":", 2, ' ') . str_pad($header->nim .' '. $header->nama 			, 87, ' ', STR_PAD_RIGHT).$_lf;
-	$hdr .= str_pad("JURUSAN", 15,' ') . 	str_pad(":", 2, ' ') . str_pad($header->jurusan 							, 87, ' ', STR_PAD_RIGHT).$_lf;
-	$hdr .= str_pad("BATAS STUDI", 15,' ') .str_pad(":", 2, ' ') . str_pad($header->batas_studi  						, 87, ' ', STR_PAD_RIGHT).$_lf;
-	$hdr .= str_pad("P.A.", 15,' ') . 		str_pad(":", 2, ' ') . str_pad($header->dosen_pembimbing					, 87, ' ', STR_PAD_RIGHT).$_lf;
+	$hdr .= str_pad("NIM & NAMA", 15, ' ') .
+          str_pad(":", 2, ' ') .
+          str_pad($header->nim .' '. $header->nama 			, 87, ' ', STR_PAD_RIGHT) .
+          $_lf;
+	$hdr .= str_pad("JURUSAN", 15,' ') .
+          str_pad(":", 2, ' ') .
+          str_pad($header->jurusan , 87, ' ', STR_PAD_RIGHT) .
+          $_lf;
+	$hdr .= str_pad("BATAS STUDI", 15,' ') .
+          str_pad(":", 2, ' ') .
+          str_pad($header->batas_studi, 87, ' ', STR_PAD_RIGHT) .
+          $_lf;
+	$hdr .= str_pad("P.A.", 15,' ') .
+          str_pad(":", 2, ' ') .
+          str_pad($header->dosen_pembimbing	, 87, ' ', STR_PAD_RIGHT) .
+          $_lf;
 	$hdr .= $line;
 
 	// TITLE
@@ -102,30 +114,31 @@ function createFile()
 
 	$hdr .= $line;
 
-  	foreach(json_decode($json) as $row) {
-  		$n++;
-  		$hdr .= str_pad($n.'.', 				6) .
-	      		str_pad($row->kode_matakuliah, 	8) .
-	      		str_pad($row->nama_matakuliah, 	39).
-	      		str_pad($row->sks, 				7) .
-	      		str_pad($row->semester, 		9) .
-	      		str_pad($row->nilai, 			4) .
-				str_pad($row->bobot, 			8, ' ', STR_PAD_LEFT) .
-				str_pad($row->nxk, 				9, ' ', STR_PAD_LEFT) .
-      		$_lf;
-  	}
+	foreach(json_decode($json) as $row) {
+		$n++;
+		$hdr .= str_pad($n.'.', 6) .
+      		str_pad($row->kode_matakuliah, 	8) .
+      		str_pad($row->nama_matakuliah, 	39).
+      		str_pad($row->sks, 7) .
+      		str_pad($row->semester, 9) .
+      		str_pad($row->nilai, 4) .
+			    str_pad($row->bobot, 8, ' ', STR_PAD_LEFT) .
+			    str_pad($row->nxk, 9, ' ', STR_PAD_LEFT) .
+    		$_lf;
+	}
 
-  	$hdr .= $line;
+  $hdr .= $line;
 	$hdr .= str_pad("IPK  " . number_format($header->ipk, 2),	37,	' ',	STR_PAD_LEFT) .
-			str_pad($header->sks_total ,	17,' ',STR_PAD_LEFT).
-			str_pad($header->nxk 	   ,	35,' ',STR_PAD_LEFT).$_lf;
+			    str_pad($header->sks_total ,	17,' ',STR_PAD_LEFT).
+			    str_pad($header->nxk 	   ,	35,' ',STR_PAD_LEFT).$_lf;
 
 	$hdr .= $line;
-	$hdr .= str_pad("Dicetak oleh : Hans Solo", 20, ' ') . str_pad("Dicetak Tgl : " . date('d-m-Y'), 60,' ', STR_PAD_LEFT).$_lf.$_lf;
+	$hdr .= str_pad("Dicetak oleh : Hans Solo", 20, ' ') .
+          str_pad("Dicetak Tgl : " . date('d-m-Y'), 60,' ', STR_PAD_LEFT).$_lf.$_lf;
 	$hdr .= chr(12);
 
 	// Force download
-  	header('Content-disposition: attachment; filename=laporan_ipk.txt');
+  header('Content-disposition: attachment; filename=laporan_ipk.txt');
 	header('Content-type: text/plain');
 
 	echo $hdr;
